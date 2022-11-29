dd9357d808eee60a9f6d8098da4136f27e06b6e6
Roman <bashkatovr@mail.ru>
2022-11-29 15:42:57 +0300

    задание 3

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,26 @@ public class Main {
     public static void main(String[] args) {
      //Задание 1
         System.out.println("Задание 1");
-        int[] arr1= new int[3];
-        arr1[0]=1;
-        arr1[1]=2;
-        arr1[2]=3;
+        int[] arr1= {1,2,3};
         double[] arr2= {1.57, 7.654, 9.986};
         int[] arr3= {1,2,3,4,5,6,7,8};
-        for (int i = 0; i < arr1.length; i++) {
+        for (int i = arr1.length-1; i >= 0; i--) {
             System.out.println(arr1[i]);
-            if (i!=arr1.length-1) {
+            if (i!=0) {
                 System.out.println(", ");
             }
         }
         System.out.println();
-        for (int i = 0; i < arr2.length; i++) {
+        for (int i = arr2.length-1; i >= 0; i--) {
             System.out.println(arr2[i]);
-            if (i!=arr2.length-1) {
+            if (i!=0) {
                 System.out.println(", ");
             }
         }
         System.out.println();
-        for (int i = 0; i < arr3.length; i++) {
+        for (int i =arr3.length-1; i >= 0; i--) {
             System.out.println(arr3[i]);
-            if (i!=arr3.length-1) {
+            if (i!=0) {
                 System.out.println(", ");
             }
         }
